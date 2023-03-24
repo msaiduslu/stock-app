337a66ca8122eabb607c50bc93df10d7777e4ccf
Muhammed Said USLU <msaiduslu@gmail.com>
2023-03-24 22:35:47 +0300

    started to create stock state managements

--- src/app/store.jsx
+++ src/app/store.jsx
@@ -1,10 +1,23 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/authSlice";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
+import stockReducer from "../features/stockSlice";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, authReducer);
 
 const store = configureStore({
   reducer: {
-    auth: authReducer,
+    auth: persistedReducer,
+    stock: stockReducer,
   },
   devTools: process.env.NODE_ENV !== "production",
 });
+
+export const persistor = persistStore(store);
 export default store;
