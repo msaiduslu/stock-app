6b9dcaa01b78e7212ee5398da5d6142cdeaabd34
Muhammed Said USLU <msaiduslu@gmail.com>
2023-03-24 22:35:04 +0300

    dashboard and sidebar

---
+++ src/components/MenuListItems.jsx
@@ -0,0 +1,41 @@
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import { useNavigate } from "react-router-dom";
+import { icons } from "../helper/DashboardIcons";
+
+const iconStyle = {
+  color: "white",
+  "& .MuiSvgIcon-root": { color: "white" },
+  "&:hover": { color: "red" },
+  "&:hover .MuiSvgIcon-root": { color: "red" },
+};
+const MenuListItems = () => {
+  const navigate = useNavigate();
+  return (
+    <div>
+      <List>
+        {icons?.map((item, index) => (
+          <ListItem key={index} disablePadding>
+            {item.url.includes("http") && (
+              <ListItemButton to={item.url} sx={iconStyle}>
+                <ListItemIcon>{item.icon}</ListItemIcon>
+                <ListItemText primary={item.title} />
+              </ListItemButton>
+            )}
+            {!item.url.includes("http") && (
+              <ListItemButton onClick={() => navigate(item.url)} sx={iconStyle}>
+                <ListItemIcon>{item.icon}</ListItemIcon>
+                <ListItemText primary={item.title} />
+              </ListItemButton>
+            )}
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  );
+};
+
+export default MenuListItems;
---
+++ src/helper/DashboardIcons.js
@@ -0,0 +1,45 @@
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import AttachMoneyIcon from "@mui/icons-material/AttachMoney";
+import InventoryIcon from "@mui/icons-material/Inventory";
+import StoreIcon from "@mui/icons-material/Store";
+import StarsIcon from "@mui/icons-material/Stars";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import SupervisorAccountIcon from "@mui/icons-material/SupervisorAccount";
+
+export const icons = [
+  {
+    icon: <DashboardIcon />,
+    title: "Dashboard",
+    url: "/stock/",
+  },
+  {
+    title: "Purchase",
+    icon: <ShoppingCartIcon />,
+    url: "/stock/purchases/",
+  },
+  {
+    title: "Sales",
+    icon: <AttachMoneyIcon />,
+    url: "/stock/sales/",
+  },
+  {
+    title: "Firms",
+    icon: <StoreIcon />,
+    url: "/stock/firms/",
+  },
+  {
+    title: "Brands",
+    icon: <StarsIcon />,
+    url: "/stock/brands/",
+  },
+  {
+    title: "Products",
+    icon: <InventoryIcon />,
+    url: "/stock/products/",
+  },
+  {
+    title: "Admin Panel",
+    icon: <SupervisorAccountIcon />,
+    url: "https://10001.fullstack.clarusway.com/admin",
+  },
+];
