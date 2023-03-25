ff4d1e8753b6eea3fc35045ddbddb233a0f98e5b
Muhammed Said USLU <msaiduslu@gmail.com>
2023-03-25 10:59:19 +0300

    register and logout functions created.

--- src/hooks/useAuthCall.jsx
+++ src/hooks/useAuthCall.jsx
@@ -1,13 +1,20 @@
 import axios from "axios";
 import { useDispatch } from "react-redux";
-import { fetchFail, fetchStart, loginSuccess } from "../features/authSlice";
+import {
+  fetchFail,
+  fetchStart,
+  loginSuccess,
+  logoutSuccess,
+  registerSuccess,
+} from "../features/authSlice";
 import { useNavigate } from "react-router-dom";
+import { toastErrorNotify, toastSuccessNotify } from "../helper/ToastNotify";
 
 const useAuthCall = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const BASE_URL = "https://12182.fullstack.clarusway.com/";
   const login = async (userInfo) => {
-    const BASE_URL = "https://12182.fullstack.clarusway.com/";
     dispatch(fetchStart());
     try {
       const { data } = await axios.post(
@@ -15,15 +22,45 @@ const useAuthCall = () => {
         userInfo
       );
       dispatch(loginSuccess(data));
+      toastSuccessNotify("Login Successful");
       navigate("/stock");
-      console.log(data);
       return data;
     } catch (error) {
       dispatch(fetchFail());
+      toastErrorNotify("Login Failed");
     }
   };
 
-  return { login };
+  const register = async (userInfo) => {
+    dispatch(fetchStart());
+    try {
+      const { data } = await axios.post(
+        `${BASE_URL}account/register/`,
+        userInfo
+      );
+      dispatch(registerSuccess(data));
+      toastSuccessNotify("Register Successful");
+      navigate("/stock");
+    } catch (error) {
+      dispatch(fetchFail());
+      toastErrorNotify("Register Failed");
+    }
+  };
+
+  const logout = async () => {
+    dispatch(fetchStart());
+    try {
+      await axios.post(`${BASE_URL}account/auth/logout/`);
+      dispatch(logoutSuccess());
+      toastSuccessNotify("Logout Successful");
+      navigate("/");
+    } catch (error) {
+      dispatch(fetchFail());
+      toastErrorNotify("Logout Failed");
+    }
+  };
+
+  return { logout, login, register };
 };
 
 export default useAuthCall;
