ee66a776603c9098b262a4b889f9542d80e8e5d6
Muhammed Said USLU <msaiduslu@gmail.com>
2023-03-25 10:59:52 +0300

    register form created.

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -4,13 +4,34 @@ import Avatar from "@mui/material/Avatar";
 import LockIcon from "@mui/icons-material/Lock";
 import image from "../assets/result.svg";
 import Grid from "@mui/material/Grid";
-import { Link, useNavigate } from "react-router-dom";
-import { Box } from "@mui/material";
+import { Link } from "react-router-dom";
+import { Box, TextField } from "@mui/material";
 import { useSelector } from "react-redux";
+import { Formik, Form } from "formik";
+import { LoadingButton } from "@mui/lab";
+import { object, string } from "yup";
+import useAuthCall from "../hooks/useAuthCall";
 
 const Register = () => {
-  const navigate = useNavigate();
-  const { currentUser, error } = useSelector((state) => state.auth);
+  const { loading } = useSelector((state) => state.auth);
+  const { register } = useAuthCall();
+  const loginSchema = object({
+    username: string().max(10, "must be below 10 character").required(),
+    first_name: string().max(20, "must be below 20 character").required(),
+    last_name: string().max(20, "must be below 20 character").required(),
+    email: string().email().required(),
+    password: string()
+      .required()
+      .min(8)
+      .max(20)
+      .matches(/\d+/, "password must be containe at least one number")
+      .matches(/[a-z]/, "password must be containe at least one lowercase")
+      .matches(/[A-Z]/, "password must be containe at least one uppercase")
+      .matches(
+        /[!,?{}<>%$#^&*+-.]+/,
+        "password must be containe at least one special character"
+      ),
+  });
 
   return (
     <Container maxWidth="lg">
@@ -49,7 +70,95 @@ const Register = () => {
           >
             Register
           </Typography>
-
+          <Formik
+            initialValues={{
+              username: "",
+              first_name: "",
+              last_name: "",
+              email: "",
+              password: "",
+            }}
+            validationSchema={loginSchema}
+            onSubmit={(values, actions) => {
+              register({ ...values, password2: values.password });
+              actions.resetForm();
+              actions.setSubmitting(false);
+            }}
+          >
+            {({ values, handleChange, handleBlur, errors, touched }) => (
+              <Form>
+                <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
+                  <TextField
+                    label="Username"
+                    name="username"
+                    id="username"
+                    type="text"
+                    variant="outlined"
+                    value={values?.username || ""}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    error={touched.username && Boolean(errors.username)}
+                    helperText={touched.username && errors.username}
+                  />
+                  <TextField
+                    label="First Name"
+                    name="first_name"
+                    id="first_name"
+                    type="text"
+                    variant="outlined"
+                    value={values?.first_name || ""}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    error={touched.first_name && Boolean(errors.first_name)}
+                    helperText={touched.first_name && errors.first_name}
+                  />
+                  <TextField
+                    label="Last Name"
+                    name="last_name"
+                    id="last_name"
+                    type="text"
+                    variant="outlined"
+                    value={values?.last_name || ""}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    error={touched.last_name && Boolean(errors.last_name)}
+                    helperText={touched.last_name && errors.last_name}
+                  />
+                  <TextField
+                    label="Email"
+                    name="email"
+                    id="email"
+                    type="email"
+                    variant="outlined"
+                    value={values?.email || ""}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    error={touched.email && Boolean(errors.email)}
+                    helperText={touched.email && errors.email}
+                  />
+                  <TextField
+                    label="Password"
+                    name="password"
+                    id="password"
+                    type="password"
+                    variant="outlined"
+                    value={values?.password || ""}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    error={touched.password && Boolean(errors.password)}
+                    helperText={touched.password && errors.password}
+                  />
+                  <LoadingButton
+                    variant="contained"
+                    type="submit"
+                    loading={loading}
+                  >
+                    Submit
+                  </LoadingButton>
+                </Box>
+              </Form>
+            )}
+          </Formik>
           <Box sx={{ textAlign: "center", mt: 2 }}>
             <Link to="/">Do you have an account?</Link>
           </Box>
