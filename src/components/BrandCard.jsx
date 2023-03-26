a461d5b8fae46a6a78d6957d1c9967585195a28d
Muhammed Said USLU <msaiduslu@gmail.com>
2023-03-26 21:53:22 +0300

    brand card and brand page created with API

---
+++ src/components/BrandCard.jsx
@@ -0,0 +1,45 @@
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
+import EditIcon from "@mui/icons-material/Edit";
+import Typography from "@mui/material/Typography";
+import { btnStyle, flex } from "../styles/globalStyle";
+import useStockCall from "../hooks/useStockCall";
+
+export default function BrandCard({ brand }) {
+  const { deleteStockData } = useStockCall();
+  return (
+    <Card
+      sx={{
+        p: 2,
+        width: "300px",
+        height: "400px",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+      }}
+    >
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {brand?.name}
+        </Typography>
+      </CardContent>
+      <CardMedia
+        image={brand?.image}
+        sx={{ p: 1, objectFit: "contain", height: "130px" }}
+        component="img"
+        alt="brand-img"
+      />
+      <CardActions sx={flex}>
+        <EditIcon sx={btnStyle} />
+        <DeleteOutlineIcon
+          sx={btnStyle}
+          onClick={() => deleteStockData("brands", brand.id)}
+        />
+      </CardActions>
+    </Card>
+  );
+}
--- src/pages/Brands.jsx
+++ src/pages/Brands.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import BrandCard from "../components/BrandCard";
+import { Button, Grid, Typography } from "@mui/material";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import useStockCall from "../hooks/useStockCall";
+import { flex } from "../styles/globalStyle";
 
 const Brands = () => {
-  return <div>Brands</div>;
+  const { brands } = useSelector((state) => state.stock);
+  const { getStockData } = useStockCall();
+  useEffect(() => {
+    getStockData("brands");
+  }, []);
+  return (
+    <div>
+      <Typography variant="h4" color="error" mb={3}>
+        Firms
+      </Typography>
+      <Button variant="contained">New Firm</Button>
+      <Grid container sx={flex}>
+        {brands?.map((brand) => (
+          <Grid key={brand.id} item>
+            <BrandCard brand={brand} />
+          </Grid>
+        ))}
+      </Grid>
+    </div>
+  );
 };
 
 export default Brands;
