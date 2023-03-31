51800c56d4f96f36ef04f5ddc38b028338235593
Muhammed Said USLU <msaiduslu@gmail.com>
2023-04-01 00:34:47 +0300

    home page created

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import { Typography } from "@mui/material";
+import React, { useEffect } from "react";
+import Charts from "../components/Charts";
+import KpiCards from "../components/KpiCards";
+import useStockCall from "../hooks/useStockCall";
 
 const Home = () => {
-  return <div>Home</div>;
+  const { getStockData } = useStockCall();
+  useEffect(() => {
+    getStockData("sales");
+    getStockData("purchases");
+  }, []);
+
+  return (
+    <div>
+      <Typography variant="h4" color="error" mb={3}>
+        Dashboard
+      </Typography>
+
+      <KpiCards />
+      <Charts />
+    </div>
+  );
 };
 
 export default Home;
